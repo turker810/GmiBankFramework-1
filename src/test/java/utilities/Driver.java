34873897b93cee51fc8c5b6c220ed770a6668a7f
cup-tan <ctable372@gmail.com>
2021-10-03 13:15:03 -0400

    second

---
+++ src/test/java/utilities/Driver.java
@@ -0,0 +1,44 @@
+package utilities;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import java.util.concurrent.TimeUnit;
+public class Driver {
+    private Driver(){
+    }
+    private static WebDriver driver;
+    public static WebDriver getDriver(){
+        if(driver==null) {
+            switch (ConfigReader.getProperty("browser")) {
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                    break;
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                    break;
+                case "ie":
+                    WebDriverManager.iedriver().setup();
+                    driver = new InternetExplorerDriver();
+                    break;
+                case "chrome-headless":
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver(new ChromeOptions().setHeadless(true));
+                    break;
+            }
+        }
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        return driver;
+    }
+    public static void closeDriver(){
+        if (driver!=null) {
+            driver.quit();
+            driver=null;
+        }
+    }
+}
