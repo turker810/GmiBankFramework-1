7ab14c34c9588c5d4bd57247461b70b49803634b
cup-tan <ctable372@gmail.com>
2021-10-04 01:35:12 -0400

    second

--- src/test/java/com/gmibank/step_definitions/Hooks.java
+++ src/test/java/com/gmibank/step_definitions/Hooks.java
@@ -1,2 +1,40 @@
-package com.gmibank.step_definitions;public class Hooks {
-}
+package com.gmibank.step_definitions;
+
+import com.gmibank.utilities.Driver;
+import cucumber.api.Scenario;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+public class Hooks {
+
+
+        @Before
+        public void setUpScenario() {
+            System.out.println("----> Before annotation: Setting up browser");
+        }
+
+        // @Before(value = "@db", order = 1)
+        public void setUpDataBaseConnection() {
+            System.out.println("------>BEFORE ANNOTATION: DB CONNECTION CREATED <------");
+        }
+
+        @After(order = 1)
+        public void tearDownScenario(Scenario scenario) { // coming from scenario class
+            //  System.out.println("----> After annotation: Closing browser");
+            System.out.println("scenario.getName() = " + scenario.getName());
+
+            if (scenario.isFailed()) {
+                try {
+                    byte[] screenshot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+                    scenario.embed(screenshot, "image/png");
+                    scenario.getStatus();
+                    System.out.println("scenario.getName() = " + scenario.getName());
+                    scenario.write("This test is written to the report..");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
