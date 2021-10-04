2849bc0eae129e5462854366ee203feaf8aabcd4
cup-tan <ctable372@gmail.com>
2021-10-04 15:06:00 -0400

    Added plugins in Runner class.

--- src/test/java/com/gmibank/runners/Runner.java
+++ src/test/java/com/gmibank/runners/Runner.java
@@ -1,7 +1,24 @@
 package com.gmibank.runners;
 
-public class Runner {
-    public static void main(String[] args) {
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
 
-    }
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "html:target/default-cucumber-reports",
+                "json:target/json-report/cucumber.json",
+                "junit:target/xml-report/cucumber.xml",
+                "rerun:target/failedRerun.txt"
+        },
+        features = "src/test/resources/features",
+        glue = "com/gmibank/step_definitions",
+        //tags = "",
+        dryRun = false
+)
+
+public class Runner {
+
 }
